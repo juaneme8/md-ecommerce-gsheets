ac3881fd11c5ff6e6fc6ade2b3a077725535fa7a
Juan Ocho <juaneme8@gmail.com>
2021-04-18 00:44:07 -0300

    API methods and  JSON stringified in screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
 import { GetStaticProps } from 'next';
 import React from 'react';
 import { Product } from '../product/types';
-
+import api from '../product/api';
 interface Props {
 	products: Product[];
 }
 
-const IndexRoute: React.FC<Props> = () => <div>{`<IndexRoute! />`}</div>;
+const IndexRoute: React.FC<Props> = ({ products }) => <div>{JSON.stringify(products)}</div>;
 
 export const getStaticProps: GetStaticProps = async () => {
+	const products = await api.list();
 	return {
 		props: {
-			products: [],
+			products,
 		},
 	};
 };
