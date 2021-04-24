7080e98ddb6d91984ea71e738d1c7103851f5876
Juan Ocho <juaneme8@gmail.com>
2021-04-24 18:28:04 -0300

    Maqueta y checkout de cart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Grid, GridItem } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { Grid, Link, Stack, Text } from '@chakra-ui/layout';
 import { GetStaticProps } from 'next';
 
 import api from '../product/api';
@@ -9,15 +10,81 @@ interface Props {
   products: Product[];
 }
 
-const IndexRoute: React.FC<Props> = ({ products }) => (
-  <Grid gap={6} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
-    {products.map((product) => (
-      <GridItem key={product.id} bg="gray.300">
-        {product.title}
-      </GridItem>
-    ))}
-  </Grid>
-);
+const parseCurrency = (value: number): string => {
+  return value.toLocaleString('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+  });
+};
+
+const IndexRoute: React.FC<Props> = ({ products }) => {
+  const [cart, setCart] = React.useState<Product[]>([]);
+
+  const text = React.useMemo(() => {
+    return cart
+      .reduce(
+        (message, product) =>
+          message.concat(
+            `* ${product.title} - ${parseCurrency(
+              product.price
+            )}\n`
+          ),
+        ``
+      )
+      .concat(
+        `\nTotal: ${parseCurrency(
+          cart.reduce(
+            (total, product) => (total += product.price),
+            0
+          )
+        )}`
+      );
+  }, [cart]);
+
+  const handleAddToCart = (product: Product) => {
+    setCart((cart) => cart.concat(product));
+  };
+
+  return (
+    <Stack>
+      <Grid
+        gap={6}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        {products.map((product) => (
+          <Stack key={product.id} bg="gray.300">
+            <Text>{product.title}</Text>
+            <Text>{parseCurrency(product.price)}</Text>
+            <Button
+              colorScheme="pink"
+              onClick={() => handleAddToCart(product)}
+            >
+              Agregar
+            </Button>
+          </Stack>
+        ))}
+      </Grid>
+
+      {Boolean(cart.length) && (
+        <>
+          <Button
+            isExternal
+            as={Link}
+            colorScheme="pink"
+            href={`https://wa.me/5491150065693?text=${encodeURIComponent(
+              text
+            )}`}
+          >
+            Completar pedido
+          </Button>
+          <Text bg="pink.100" color="black">
+            {text}
+          </Text>
+        </>
+      )}
+    </Stack>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const products = await api.list();
