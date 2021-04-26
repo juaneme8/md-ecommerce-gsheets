64f27d585318ddc938509899b3e2457062393f6e
Juan Ocho <juaneme8@gmail.com>
2021-04-25 22:49:42 -0300

    ✨ Agregado de position sticky en botón de checkout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { Grid, Link, Stack, Text } from '@chakra-ui/layout';
-import { Button } from '@chakra-ui/react';
+import { Grid, Link, Stack, Text, Button, Flex } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
 
 import api from '../product/api';
@@ -31,13 +30,17 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
   };
 
   return (
-    <Stack>
+    <Stack spacing={6}>
       <Grid gap={6} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
         {products.map((product) => (
-          <Stack key={product.id} bg="gray.100">
-            <Text>{product.title}</Text>
-            <Text>{parseCurrency(product.price)}</Text>
-            <Button colorScheme="primary" onClick={() => handleAddToCart(product)}>
+          <Stack key={product.id} bg="gray.100" borderRadius="md" padding={4}>
+            <Stack spacing={1}>
+              <Text>{product.title}</Text>
+              <Text color="green.500" fontSize="sm">
+                {parseCurrency(product.price)}
+              </Text>
+            </Stack>
+            <Button colorScheme="primary" size="sm" variant="outline" onClick={() => handleAddToCart(product)}>
               Agregar
             </Button>
           </Stack>
@@ -45,19 +48,16 @@ const IndexRoute: React.FC<Props> = ({ products }) => {
       </Grid>
 
       {Boolean(cart.length) && (
-        <>
+        <Flex align="center" bottom={4} justify="center" position="sticky">
           <Button
             isExternal
             as={Link}
-            colorScheme="primary"
+            colorScheme="whatsapp"
             href={`https://wa.me/5491150065693?text=${encodeURIComponent(text)}`}
           >
-            Completar pedido
+            Completar pedido ({cart.length} productos)
           </Button>
-          <Text bg="primary.300" color="black">
-            {text}
-          </Text>
-        </>
+        </Flex>
       )}
     </Stack>
   );
