fb5918bb22c04e17f21461d98c049be05628da7b
Juan Ocho <juaneme8@gmail.com>
2022-04-16 20:32:52 -0300

    Minor changes to styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <Box padding={4}>
-        <Container backgroundColor="white" borderRadius="sm" boxShadow="md" maxWidth="container.xl" padding={4}>
+        <Container backgroundColor="gray.800" borderRadius="sm" boxShadow="md" maxWidth="container.xl" padding={4}>
           <VStack mb={4}>
             <Image borderRadius="9999" src="assets/avatar.jpg" />
             <Heading>Juaneme8</Heading>
